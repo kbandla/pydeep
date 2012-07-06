1a1e20c872c88aceff2b7ebe0408751be2cd8c83
Kiran Bandla <kbandla@in2void.com>
2012-07-06 10:25:53 -0400

    Deleting binaries used for testing.
        *   Deleting zeus binaries used for testing ssdeep hashes.
        *   Updated test script for new,clean binaries [ calc.exe, notepad.exe ]

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,12 @@
 import pydeep
-file1 = 'zeus_2bad405e5e70f12756cd80cb14760139'
-file2 = 'zeus_9b2de8b062a5538d2a126ba93835d1e9'
-file1hash = '3072:/caqyte6gV77snHLLxtDyaXOqdPNbnhW4IxZx5kCZuubFrhU1wKKrONmxt:/caBt277snHRUY7PNNW4IxZ7zbC0rONm'
-file2hash = '3072:J9GgqeRehDMVYQKSGJhJX11o0wojolTmXJmfEaQHNo8+PZ7ya4aMi4ry0zxLbnJG:J9JqeohDMODSGFX11o0wo0AJ4+a82Z7U'
+file1 = 'calc.exe'
+file2 = 'notepad.exe'
+file1hash = '1536:JEl14rQcWAkN7GAlqbkfAGQGV8aMbrNyrf1w+noPvLV6eBsCXKc:JYmZWXyaiedMbrN6pnoXL1BsC'
+file2hash = '1536:0awOnbNQKLjWDyy1o5RefYMJUEbooPRrKKRl1P3:0YNQKPWDyDRefVJltZrpRl1P3'
 data1 = open(file1).read()
 data2 = open(file2).read()
-assert len(data1) == 141312, "File length error"
-assert len(data2) ==  194560, "File lendth error"
+assert len(data1) == 114688, "File length error"
+assert len(data2) ==  69120, "File lendth error"
 hash01 = pydeep.hash_buf(data1)
 hash02 = pydeep.hash_buf(data2)
 assert hash01 == file1hash, "Error hashing file1"
